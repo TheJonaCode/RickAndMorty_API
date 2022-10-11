342976665979a825de5e9b8d7a2c352f44e2b31b
TheJonaCode <mcjhona069@gmail.com>
2022-10-11 18:50:43 -0500

    fetch v2

--- js/main.js
+++ js/main.js
@@ -1,40 +1,46 @@
 const spinner = document.querySelector('#spinner');
 
-function getCharacters(done) {
-    const results = fetch('https://rickandmortyapi.com/api/character/?page=24');
-    results
-        .then(response => response.json())
-        .then(data => {
-            done(data);
-        });
-    /*Spinner se muestra*/
-    spinner.style.display = "block";
+let offset = 1;
+let limit = 11;
+
+async function fetchCharacter(id) {
+    const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
+    const data = await res.json();
+    createCharacter(data);
+    /*Spinner oculto*/
+    spinner.style.display = "none";
 }
 
-getCharacters(data => {
+function fetchCharacters(offset, limit) {
+    /*Spinner se muestra*/
+    spinner.style.display = "block";
+    for (let i = offset; i <= offset + limit; i++) {
+        fetchCharacter(i);
+    }
+}
 
-    data.results.forEach(personaje => {
-        const character__card = document.createRange().createContextualFragment(
-            /*HTML*/
-            `
-            <div class="character__card">
-                <div class="img__container">
-                    <img src="${personaje.image}" alt="Personaje">
-                </div>
-                <div class="info__container">
-                    <p class="character__name">${personaje.name}</p>
-                    <p class="character__type">${personaje.species}</p>
-                    <p class="character__status">${personaje.status}</p>
-                </div>
+function createCharacter(personaje) {
+    const character__card = document.createRange().createContextualFragment(
+        /*HTML*/
+        `
+        <div class="character__card">
+            <div class="img__container">
+                <img src="${personaje.image}" alt="Personaje">
             </div>
-            `
-        );
+            <div class="info__container">
+                <p class="character__name">${personaje.name}</p>
+                <p class="character__type">${personaje.species}</p>
+                <p class="character__status">${personaje.status}</p>
+            </div>
+        </div>
+        `
+    );
 
-        const main = document.querySelector('.contenedor');
-        main.append(character__card);
+    const main = document.querySelector('.contenedor');
+    main.append(character__card);
 
-        /*Spinner oculto*/
-        spinner.style.display = "none";
+    /*Spinner oculto*/
+    spinner.style.display = "none";
+}
 
-    });
-});
+fetchCharacters(offset, limit);
